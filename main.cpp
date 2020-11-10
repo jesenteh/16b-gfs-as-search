ef44822ccdb84f0ab28624ce1d6cffd9c4fd784b
jesenteh <64997466+jesenteh@users.noreply.github.com>
2020-11-10 21:07:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
  * The following program will generate a file that contains truncated differences, the number of rounds, the number of AS and permutation pattern for each sample.
  * Special thanks to Yeoh Wei Zhu (https://github.com/yeohweizhu) and Lee Ting Rong (https://github.com/trlee) for earlier versions of the code
  * Note that the program may have redundant code from the original full differential search algorithm
- * Instructions:
- * 1. Modify the variable NROUNDS in common.h to specify the number of rounds for the search.
- * 2. Modify the permutation pattern in trail_as_search.cpp to specify the 16-branch permutation used for the search.
- * 3. Search for the "!!" symbol in as_search.cpp to see where to modify the search space
+ * All parameters to control the program are under the "Control Panel" in common.h
  * Output file columns : Input truncated diff, output truncated diff, active s-boxes , number of rounds, permutation pattern (as 16 columns)
  */
 
