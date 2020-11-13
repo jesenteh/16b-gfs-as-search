ff308d02dcdd2a78011b2ce565b000653a1505e7
jesenteh <64997466+jesenteh@users.noreply.github.com>
2020-11-13 08:57:07 +0800

    Update trail_as_search.cpp

--- trail_as_search.cpp
+++ trail_as_search.cpp
@@ -15,7 +15,7 @@ std::map<string, int> truncSet;
 
 long globalCount = 0;
 
-
+unsigned int trunc_diff[10000]={};
 
 std::string perm_str = "";
 std::string file_name = "training-data-";
@@ -37,77 +37,111 @@ void roundProcess(const int n, const int nrounds, int as, int B[], int* Bn,
 	//6 - 0x20	14- 0x2000
 	//7 - 0x40	15- 0x4000
 	//8 - 0x80	16- 0x8000
-	for (int i=0;i<16;i++){
-		if (perm[i] < i){
-			new_diff = new_diff | ( (ori & (0x1 << i)) >> (i-perm[i]) );
-		}else{
-			new_diff = new_diff | ( (ori & (0x1 << i)) << (perm[i]-i) );
-		}
+	int p[16]={};
+	for (int i=0; i<16; i++){
+        p[i]=perm[i]-i;
 	}
 
-    int pos[16] = {0};
-    //Generate Mask
-	if (((ori&0x2)!=0) && ((ori&0x1)!=0)){
-        mask = mask | 0x2;
-		pos[hw] = 2;
-        hw++;
-	}
-	if (((ori&0x8)!=0) && ((ori&0x4)!=0)){
-        mask = mask | 0x8;
-		pos[hw] = 4;
-        hw++;
-	}
-	if (((ori&0x20)!=0) && ((ori&0x10)!=0)){
-        mask = mask | 0x20;
-		pos[hw] = 6;
-        hw++;
-	}
-	if (((ori&0x80)!=0) && ((ori&0x40)!=0)){
-        mask = mask | 0x80;
-		pos[hw] = 8;
-        hw++;
-	}
 
-	if (((ori&0x200)!=0) && ((ori&0x100)!=0)){
-        mask = mask | 0x200;
-		pos[hw] = 10;
-        hw++;
-	}
-	if (((ori&0x800)!=0) && ((ori&0x400)!=0)){
-        mask = mask | 0x800;
-		pos[hw] = 12;
-        hw++;
-	}
-
-	if (((ori&0x2000)!=0) && ((ori&0x1000)!=0)){
-        mask = mask | 0x2000;
-		pos[hw] = 14;
-        hw++;
-	}
+	//Bit position 0 and 1
+	new_diff = new_diff | ((ori&0x8000)>>p[0]); //perm[0]-0 is always larger than 0, so shift right
+	if (p[1]>=0) new_diff = new_diff | ((((ori&0x8000)>>1) ^ (ori&0x4000))>>p[1]);
+	else new_diff = new_diff | ((((ori&0x8000)>>1) ^ (ori&0x4000))<<abs(p[1]));
 	if (((ori&0x8000)!=0) && ((ori&0x4000)!=0)){
-        mask = mask | 0x8000;
-		pos[hw] = 16;
+        if (p[1]>=0) mask = mask | (0x4000>>p[1]);
+        else mask = mask | (0x4000<<abs(p[1]));
+        hw++;
+	}
+
+    //Bit position 2 and 3
+	if (p[2]>=0) new_diff = new_diff | ((ori&0x2000)>>p[2]);
+	else new_diff = new_diff | ((ori&0x2000)<<abs(p[2]));
+	if (p[3]>=0) new_diff = new_diff | ((((ori&0x2000)>>1) ^ (ori&0x1000))>>p[3]);
+	else new_diff = new_diff | ((((ori&0x2000)>>1) ^ (ori&0x1000))<<abs(p[3]));
+	if (((ori&0x2000)!=0) && ((ori&0x1000)!=0)){
+        if (p[3]>=0) mask = mask | (0x1000>>p[3]);
+        else mask = mask | (0x1000<<abs(p[3]));
+        hw++;
+	}
+
+    //Bit position 4 and 5
+	if (p[4]>=0) new_diff = new_diff | ((ori&0x800)>>p[4]);
+	else new_diff = new_diff | ((ori&0x800)<<abs(p[4]));
+	if (p[5]>=0) new_diff = new_diff | ((((ori&0x800)>>1) ^ (ori&0x400))>>p[5]);
+	else new_diff = new_diff | ((((ori&0x800)>>1) ^ (ori&0x400))<<abs(p[5]));
+	if (((ori&0x800)!=0) && ((ori&0x400)!=0)){
+        if (p[5]>=0) mask = mask | (0x400>>p[5]);
+        else mask = mask | (0x400<<abs(p[5]));
+        hw++;
+	}
+
+	//Bit position 6 and 7
+	if (p[6]>=0) new_diff = new_diff | ((ori&0x200)>>p[6]);
+	else new_diff = new_diff | ((ori&0x200)<<abs(p[6]));
+	if (p[7]>=0) new_diff = new_diff | ((((ori&0x200)>>1) ^ (ori&0x100))>>p[7]);
+	else new_diff = new_diff | ((((ori&0x200)>>1) ^ (ori&0x100))<<abs(p[7]));
+	if (((ori&0x200)!=0) && ((ori&0x100)!=0)){
+        if (p[7]>=0) mask = mask | (0x100>>p[7]);
+        else mask = mask | (0x100<<abs(p[7]));
+        hw++;
+	}
+
+    //Bit position 8 and 9
+	if (p[8]>=0) new_diff = new_diff | ((ori&0x80)>>p[8]);
+	else new_diff = new_diff | ((ori&0x80)<<abs(p[8]));
+	if (p[9]>=0) new_diff = new_diff | ((((ori&0x80)>>1) ^ (ori&0x40))>>p[9]);
+	else new_diff = new_diff | ((((ori&0x80)>>1) ^ (ori&0x40))<<abs(p[9]));
+	if (((ori&0x80)!=0) && ((ori&0x40)!=0)){
+        if (p[9]>=0) mask = mask | (0x40>>p[9]);
+        else mask = mask | (0x40<<abs(p[9]));
+        hw++;
+	}
+
+	//Bit position 10 and 11
+	if (p[10]>=0) new_diff = new_diff | ((ori&0x20)>>p[10]);
+	else new_diff = new_diff | ((ori&0x20)<<abs(p[10]));
+	if (p[11]>=0) new_diff = new_diff | ((((ori&0x20)>>1) ^ (ori&0x10))>>p[11]);
+	else new_diff = new_diff | ((((ori&0x20)>>1) ^ (ori&0x10))<<abs(p[11]));
+	if (((ori&0x20)!=0) && ((ori&0x10)!=0)){
+        if (p[11]>=0) mask = mask | (0x10>>p[11]);
+        else mask = mask | (0x10<<abs(p[11]));
+        hw++;
+	}
+
+    //Bit position 12 and 13
+	if (p[12]>=0) new_diff = new_diff | ((ori&0x8)>>p[12]);
+	else new_diff = new_diff | ((ori&0x8)<<abs(p[12]));
+	if (p[13]>=0) new_diff = new_diff | ((((ori&0x8)>>1) ^ (ori&0x4))>>p[13]);
+	else new_diff = new_diff | ((((ori&0x8)>>1) ^ (ori&0x4))<<abs(p[13]));
+	if (((ori&0x8)!=0) && ((ori&0x4)!=0)){
+        if (p[13]>=0) mask = mask | (0x4>>p[13]);
+        else mask = mask | (0x4<<abs(p[13]));
+        hw++;
+	}
+
+	//Bit position 14 and 15
+	if (p[14]>=0) new_diff = new_diff | ((ori&0x2)>>p[14]);
+	else new_diff = new_diff | ((ori&0x2)<<abs(p[14]));
+	if (p[15]>=0) new_diff = new_diff | ((((ori&0x2)>>1) ^ (ori&0x1))>>p[15]);
+	else new_diff = new_diff | ((((ori&0x2)>>1) ^ (ori&0x1))<<abs(p[15]));
+	if (((ori&0x2)!=0) && ((ori&0x1)!=0)){
+        if (p[15]>=0) mask = mask | (0x1>>p[15]);
+        else mask = mask | (0x1<<abs(p[15]));
         hw++;
 	}
 
     //Generate Mask End
 
 	for(unsigned i=0;i<(1<<hw);i++){
-		// unsigned int tmp_as;
-		// unsigned bitIdx = 0;
-		// for(unsigned k=0;k<4;k++){
-		// 	if(((mask>>k)&(0x1))!=0){
-		// 		tmp = tmp ^ (((i>>bitIdx)&0x1)<<k);
-		// 		bitIdx++;
-		// 	}
-		// }
+		 unsigned int tmp_as;
+		 unsigned bitIdx = 0;
+		 for(unsigned k=0;k<16;k++){
+		 	if(((mask>>k)&(0x1))!=0){
+		 		tmp = tmp ^ (((i>>bitIdx)&0x1)<<k);
+		 		bitIdx++;
+		 	}
+		 }
 
-        tmp=0;
-        for (int j=0;j<hw;j++){
-            tmp = tmp ^ (((i & (1<<j))>>j) << (pos[j]-1) );
-        }
-
-		unsigned int tmp_as;
 		tmp_as = as + hw16_check_even_pos(ori);
 
 		// only one round
@@ -125,10 +159,13 @@ void roundProcess(const int n, const int nrounds, int as, int B[], int* Bn,
             }
 
             AS = AS + internalTruncState[NROUNDS][0];
-			ofstream file;
-            file.open(file_name, ios::in | ios::app);
-            cout << "Number of samples: " << ++globalCount << endl;
-            file << std::bitset<16>(internalTruncState[0][1]) << ", " << std::bitset<16>(internalTruncState[NROUNDS][1]) << ", " << AS << ", " << NROUNDS <<  ", " << perm_str << endl;
+            if(NROUNDS==1){
+                ofstream file;
+                file.open(file_name, ios::in | ios::app);
+                cout << "Number of samples: " << ++globalCount << endl;
+                file << std::bitset<16>(internalTruncState[0][1]) << ", " << std::bitset<16>(internalTruncState[NROUNDS][1]) << ", " << AS << ", " << NROUNDS <<  ", " << perm_str << endl;
+                if (globalCount == MAX_SAMPLES) exit(0);
+            }
 		}
 
 		// Round-0 and not last round
@@ -170,11 +207,13 @@ void roundProcess(const int n, const int nrounds, int as, int B[], int* Bn,
 
 					AS = AS + internalTruncState[NROUNDS][0];
 
-
-                    ofstream file;
-                    file.open(file_name, ios::in | ios::app);
-					cout << "Number of samples: " << ++globalCount << endl;
-					file << std::bitset<16>(internalTruncState[0][1]) << ", " << std::bitset<16>(internalTruncState[NROUNDS][1]) << ", " << AS << ", " << NROUNDS <<  ", " << perm_str << endl;
+                    if(NROUNDS!=1){
+                        ofstream file;
+                        file.open(file_name, ios::in | ios::app);
+                        cout << "Number of samples: " << ++globalCount << endl;
+                        file << std::bitset<16>(internalTruncState[0][1]) << ", " << std::bitset<16>(internalTruncState[NROUNDS][1]) << ", " << AS << ", " << NROUNDS <<  ", " << perm_str << endl;
+                        if (globalCount == MAX_SAMPLES) exit(0);
+                    }
 				}
 			//}
         }
@@ -206,16 +245,8 @@ void cp_AS_threshold_search(const int n, const int nrounds, int B[NROUNDS], int*
 	if ((n == 0) && (nrounds == 1)) {
 		assert(*Bn == 2);
 		//!!Modify below to specify the search space. Remember to modify the second if statement below this one as well.
-
-        //Uncomment below to search all possible truncated differences
-        for(unsigned int diff = MIN_DIFF; diff < MAX_DIFF; diff+=INCREMENT){
-        //1 = 0000000000000001 16 = 0000000000001000 65536 = 1111111111111111
-
-        //Uncomment below to limit the search space to hamming weight of 1 on even positions only
-//		for(unsigned int diff = 1; diff < 65536; diff<<=1){
-//                if (hw16_check_even_pos(diff)==1){
-//                    continue;
-//                }
+        for(int i=0; i<5000; i++){
+                unsigned int diff = trunc_diff[i];
 
                 internalTruncState[0][0]=0;
                 internalTruncState[0][1]=diff; // plaintext differences
@@ -226,15 +257,8 @@ void cp_AS_threshold_search(const int n, const int nrounds, int B[NROUNDS], int*
 	// Round-0 and not last round
 	if ((n == 0) && (nrounds > 1)) {
 		//!!Modify below to specify the search space (along with the one above)
-
-        //Uncomment below to search all possible truncated differences
-        for(unsigned int diff = MIN_DIFF; diff < MAX_DIFF; diff+=INCREMENT){
-
-        //Uncomment below to limit the search space to hamming weight of 1 on even positions only
-//		for(unsigned int diff = 1; diff < 65536; diff<<=1){
-//				if (hw16_check_even_pos(diff)==1){
-//            		continue;
-//        		}
+        for(int i=0; i<5000; i++){
+            unsigned int diff = trunc_diff[i];
 
                 internalTruncState[0][0]=0;
                 internalTruncState[0][1]=diff; // plaintext differences
@@ -265,16 +289,30 @@ void cp_AS_threshold_search(const int n, const int nrounds, int B[NROUNDS], int*
 
 uint32_t cp_AS_search(int B[])
 {
-    for (int i=15;i>0;i--){
+    //Generate 10000 random input differences
+    srand(time(NULL));
+    unsigned int check[65536]={0};
+    for (int i=0; i<5000; i++)
+    {
+        unsigned int r = rand()%65536+1;
+        while ((check[r]!=0) || (hw16(r)>HW_LIMIT)) //Remove repetitions and limit the hW
+        {
+            r = rand()%65536+1;
+        }
+        trunc_diff[i]=r;
+        check[r]++;
+    }
+
+    for (int i=0;i<15;i++){
         perm_str += ( std::to_string(perm[i]) + ", ");
     }
-    perm_str+= std::to_string(perm[0]);
+    perm_str+= std::to_string(perm[15]);
 
     file_name+= (std::to_string(NROUNDS) + "-");
-    for (int i=15;i>0;i--){
+    for (int i=0;i<15;i++){
         file_name += (std::to_string(perm[i]));
     }
-    file_name+= std::to_string(perm[0]);
+    file_name+= std::to_string(perm[15]);
     file_name+=".csv";
 
 	// uint32_t num_rounds = NROUNDS; //UNUSED
